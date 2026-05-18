e1b03b7cb8b2fc1bc306496d4e4b4889eda83210
karentrasporte <karentrasporte@gmail.com>
2026-05-18 15:48:23 +0800

    Added single quotes in the docker login

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -14,7 +14,7 @@ class Docker implements Serializable {
         script.echo "building the docker image"
         script.withCredentials ([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "docker build -t $imageName ."
-            script.sh "echo '${script.PASS} | docker login -u ${script.USER} --password-stdin"
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
             script.sh "docker push $imageName"
         }
     }
