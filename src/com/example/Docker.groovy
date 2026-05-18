8c51901d6e36eee458bd5c293e98c21240413a1f
karentrasporte <karentrasporte@gmail.com>
2026-05-18 15:55:21 +0800

    Separate docker commands to a function

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -12,11 +12,17 @@ class Docker implements Serializable {
 
     def buildDockerImage(String imageName) {
         script.echo "building the docker image"
-        script.withCredentials ([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t $imageName ."
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin() {
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
-            script.sh "docker push $imageName"
         }
+    }
+
+    def dockerPush(String imageName) {
+        script.sh "docker push $imageName"
     }
 
 }
