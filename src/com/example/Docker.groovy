a94a9026dd6c26dc9ed5dc77875a5fa34a667757
karentrasporte <karentrasporte@gmail.com>
2026-05-18 15:46:55 +0800

    Updated withCredentials to use .script

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -12,7 +12,7 @@ class Docker implements Serializable {
 
     def buildDockerImage(String imageName) {
         script.echo "building the docker image"
-        script.withCredentials ([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        script.withCredentials ([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "docker build -t $imageName ."
             script.sh "echo '${script.PASS} | docker login -u ${script.USER} --password-stdin"
             script.sh "docker push $imageName"
